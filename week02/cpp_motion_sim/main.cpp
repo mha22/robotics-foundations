4b5c4370ea8ecaabf23bca8c7d93499c1105ec9a
Mohammad Ab <wma22330@gmail.com>
2026-06-22 17:22:39 +0400

    Add command-line argument parsing for motion simulation

--- week02/cpp_motion_sim/main.cpp
+++ week02/cpp_motion_sim/main.cpp
@@ -1,14 +1,26 @@
 #include "mobile_robot.hpp"
 
 #include <iostream>
+#include <string>
 
-int main() {
+int main(int argc, char* argv[]) {
     double v = 0.5;
     double w = 0.2;
     double dt = 0.1;
     int steps = 100;
-    MobileRobot robot(0.0, 0.0, 0.0);
 
+    if (argc == 5) {
+        v = std::stod(argv[1]);
+        w = std::stod(argv[2]);
+        dt = std::stod(argv[3]);
+        steps = std::stoi(argv[4]);
+    }
+    else if (argc != 1) {
+        std::cerr << "Usage: " << argv[0] << " <v> <w> <dt> <steps>" << std::endl;
+        return 1;
+    }
+
+    MobileRobot robot(0.0, 0.0, 0.0);
 
     for (int i = 0; i < steps; i++) {
         robot.move(v, w, dt);
